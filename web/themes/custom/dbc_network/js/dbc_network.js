e582a5615b775540890d16bcd0a81571fe7c0508
crazysix <brandon@crazysix.com>
2021-11-26 10:47:48 -0700

    Fix wiki menus

--- web/themes/custom/dbc_network/js/dbc_network.js
+++ web/themes/custom/dbc_network/js/dbc_network.js
@@ -4,12 +4,24 @@
       //
     }
   };
-  $('aside nav.menu--wiki li.menu__item--has-children').find('a').click(function(event){
-    event.preventDefault();
-    $(this).parent().find('ul').slideToggle('fast');
+  $('aside nav.menu--wiki li.menu__item--has-children').find('a:first').each(function( index ) {
+    if ($(this).parent().find('ul:first').css('display') == 'none') {
+      $(this).addClass('closed');
+    }
   });
-  $('.region--highlighted nav.menu--wiki li.menu__item--has-children').find('a').click(function(event){
+  $('.region--highlighted nav.menu--wiki li.menu__item--has-children').find('a:first').each(function( index ) {
+    if ($(this).parent().find('ul:first').css('display') == 'none') {
+      $(this).addClass('closed');
+    }
+  });
+  $('aside nav.menu--wiki li.menu__item--has-children').find('a:first').click(function(event){
     event.preventDefault();
-    $(this).parent().find('ul').slideToggle('fast');
+    $(this).toggleClass('closed');
+    $(this).parent().find('ul:first').slideToggle('fast');
+  });
+  $('.region--highlighted nav.menu--wiki li.menu__item--has-children').find('a:first').click(function(event){
+    event.preventDefault();
+    $(this).toggleClass('closed');
+    $(this).parent().find('ul:first').slideToggle('fast');
   });
 })(jQuery, Drupal, drupalSettings);
